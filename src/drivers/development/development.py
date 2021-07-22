938967640eec654987b200f052ec977d1b52647a
Aron <ahugoon@gmail.com>
2021-07-22 17:29:06 +0200

    Restore development driver file

--- src/drivers/development/development.py
+++ src/drivers/development/development.py
@@ -17,45 +17,16 @@
 from multiprocessing import Pipe
 from typing import Optional
 
-from ..driver import driver, VariableQuality
-import os
-import sys
-import winreg
-
-MATLAB_SDK_FOUND = False
-
-def add_matlab_path():
-    '''
-    Tries to append the path of the matlab engine.
-    If successfull, MATLAB_SDK_FOUND is set to True. Otherwise an error is raised.
-    '''
-    if os.name == 'nt': # Only try on windows
-        reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-
-        # Get registry key to the latest version of MATLAB installed
-        key = winreg.OpenKey(reg, "SOFTWARE\\MathWorks\\MATLAB")
-        matlab_version = winreg.EnumKey(key, 0)
-        key = winreg.OpenKey(reg, "SOFTWARE\\MathWorks\\MATLAB\\" + matlab_version)
-        
-        # Path to python engine relative to MATLABROOT
-        matlab_path = winreg.QueryValueEx(key, "MATLABROOT")[0] + "\\extern\\engines\\python"
-        assert os.path.isdir(matlab_path + "\\build\\lib"), "Matlab engine not built"
-
-        sys.path.append(matlab_path + "\\build\\lib")
-        
-        MATLAB_SDK_FOUND = True
+from ..driver import driver
 
 
 class development(driver):
     '''
-    This driver uses the MATLAB Engine API to connect to a shared MATLAB session. (https://mathworks.com/help/matlab/matlab-engine-for-python.html)
-
-    session_name: str
-        Specifies the name of the MATLAB session to connect to. Can be set to 'any' if there is only one shared MATLAB session.\n
-    simulink_block: str
-        Path to the block in Simulink. For example 'simulink_test/Test_Block'.\n
-    autorun_simulink: str
-        If enabled, Simulink will start running on connection and stop when disconnected.
+    Driver that can be used for development. The driver can be used on a component just assigning the driver type "development".
+    Feel free to add your code in the methods below.
+    Parameters:
+    myparam: int
+        This is just an example of a driver parameter. Default = 3
     '''
 
     def __init__(self, name: str, pipe: Optional[Pipe] = None):
@@ -67,100 +38,43 @@ class development(driver):
         driver.__init__(self, name, pipe)
 
         # Parameters
-        self.session_name = "any"
-        self.simulink_block = "simulink_session/block"
-        self.autorun_simulink = False
-
-        # Internal
-        self._runtime_object = None
-        self._rto_name = 'rto_' + self.simulink_block.replace(" ", "_").replace("/","_")
+        self.myparam = 3
 
 
     def connect(self) -> bool:
         """ Connect driver.
-
+        
         : returns: True if connection stablished False if not
         """
-        try:
-            if not MATLAB_SDK_FOUND:
-                add_matlab_path()
-                import matlab.engine
-            
-            # Get list of shared matlab sessions
-            sessions = matlab.engine.find_matlab()
-            assert sessions, "No shared matlab sessions found"
-            if self.session_id != "any":
-                # id specified, try connecting to that session
-                self._connection = matlab.engine.connect_matlab(self.session_id)
-            else:
-                self._connection = matlab.engine.connect_matlab(sessions[0])
-
-            # Start Simulink if autorun is enabled
-            if self.autorun_simulink:
-                self._connection.eval("set_param('simulink_test1','SimulationCommand','Start');", nargout=0)
-            
-            # Create temporary RuntimeObject for read/write operations
-            self._runtime_object = self._connection.eval(f"get_param('{self.simulink_block}', 'RuntimeObject');")
-            self._connection.workspace[self._rto_name] = self._runtime_object
-
-            return True
-
-        except Exception as e:
-            self.sendDebugInfo('SETUP failed: ' + str(e))
-
-        return False
+        return True
 
 
     def disconnect(self):
-        """ Disconnect driver. Remove the temporary rto variable. """
-        self._connection.eval('clear ' + self._rto_name, nargout=0)
-        if self.autorun_simulink:
-            self._connection.eval("set_param('simulink_test1','SimulationCommand','Stop');", nargout=0)
-        self._connection.exit()
+        """ Disconnect driver.
+        """
+        pass
 
 
     def addVariables(self, variables: dict):
         """ Add variables to the driver. Correctly added variables will be added to internal dictionary 'variables'.
         Any error adding a variable should be communicated to the server using sendDebugInfo() method.
-        : param variables: Variables to add in a dict following the setup format. (See documentation)
-
+        : param variables: Variables to add in a dict following the setup format. (See documentation) 
+        
         """
-        self.variables.update(variables)
-        for var_id in self.variables:
-            self.variables[var_id]['value'] = self.defaultVariableValue(self.variables[var_id]['datatype'], self.variables[var_id]['size'])
+        pass
 
 
     def readVariables(self, variables: list) -> list:
         """ Read given variable values. In case that the read is not possible or generates an error BAD quality should be returned.
-        : param variables: List of variable ids to be read.
+        : param variables: List of variable ids to be read. 
         : returns: list of tupples including (var_id, var_value, VariableQuality)
         """
-        res = []
-        for var_id in variables:
-            
-            try:
-                assert self._runtime_object, "Simulation not running"
-                new_value = self._connection.eval(self._rto_name + '.OutputPort(' + str(var_id) + ').Data')
-                if self.variables[var_id]['size'] > 1:
-                    new_value = [self.getValueFromString(self.variables[var_id]['datatype'], i) for i in new_value[0]]
-                else:
-                    new_value = self.getValueFromString(self.variables[var_id]['datatype'], new_value)
-                res.append((var_id, new_value, VariableQuality.GOOD))
-            except Exception as e:
-                res.append((var_id, self.variables[var_id]['value'], VariableQuality.BAD))
-                self.sendDebugInfo('READ failed: ' + str(e))
-
-
-        return res
+        return []
 
 
     def writeVariables(self, variables: list) -> list:
         """ Write given variable values. In case that the write is not possible or generates an error BAD quality should be returned.
-        : param variables: List of tupples with variable ids and the values to be written (var_id, var_value).
+        : param variables: List of tupples with variable ids and the values to be written (var_id, var_value). 
         : returns: list of tupples including (var_id, var_value, VariableQuality)
         """
-        res = []
-        for (var_id, new_value) in variables:
-            self._connection.workspace[var_id] = new_value
-            res.append((var_id, new_value, VariableQuality.GOOD))
-        return res
+        return []
