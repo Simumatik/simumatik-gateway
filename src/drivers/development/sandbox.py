1073042f3b35d45119ce0f068698f76693c746d3
Fabian Fasth <fabian.fasth@gmail.com>
2021-10-27 17:02:02 +0200

    sandbox testing

--- src/drivers/development/sandbox.py
+++ src/drivers/development/sandbox.py
@@ -6,7 +6,10 @@ print(plc.read_state())
 
 #print(1863*8+1)
 #plc.write(pyads.INDEXGROUP_DATA, 554749, True, pyads.PLCTYPE_BOOL)
-plc.write_by_name("ConveyorControl.vulcanConveyor.conveyorIndexer.indexerPistonsControl[0].sensors[2].hardwareInput", True)
+plc.write_by_name("ConveyorControl.vulcanConveyor.conveyorIndexer.indexerPistonsControl[0].sensors[2].hardwareInput", False)
+
+# for each message is limited to 2mb and also limit in time
+# read multiple variables. 
 
 #symbols = plc.get_symbol("ConveyorControl.vulcanConveyor.conveyorIndexer.indexerPistonsControl[0].sensors[2].hardwareInput")
 #print(symbols)
