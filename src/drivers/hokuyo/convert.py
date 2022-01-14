9449273122d721b0b6ddd19eb24a7e0ac275b20a
ahugoson <aron.hugoson@simumatik.com>
2022-01-14 14:13:03 +0100

    Encoding and CRC working

--- src/drivers/hokuyo/convert.py
+++ src/drivers/hokuyo/convert.py
@@ -1,8 +1,9 @@
 import fastcrc
+import struct
 
-str = '1AC9'
-print(f'str: {str}')
-ascii = [ord(c) for c in str]
+string = b'1AC9'.decode()
+print(f'str: {string}')
+ascii = [ord(c) for c in string]
 print(f'ascii: {ascii}')
 ascii_d = [c-0x30 if c<0x40 else c-0x37 for c in ascii]
 print(f'decimal: {ascii_d}')
@@ -10,6 +11,5 @@ binary = ''.join([format(d, '04b') for d in ascii_d])
 print(f'binary: {binary}')
 decimal = int(binary, 2)
 print(f'decimal: {decimal}')
-print(format(decimal, '04b'))
-crc = fastcrc.crc16.kermit(b'000EVR00')
-print(f'crc: {crc}')
+
+
