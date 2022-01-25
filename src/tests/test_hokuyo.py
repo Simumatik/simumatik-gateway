2bd901d46c11aeba05e9354ed9a7261af4ea994d
ahugoson <aron.hugoson@simumatik.com>
2022-01-25 17:38:24 +0100

    Make local test possible

--- src/tests/test_hokuyo.py
+++ src/tests/test_hokuyo.py
@@ -17,12 +17,42 @@
 import sys
 from os import path
 import time
+import threading
+import socket
 import numpy as np
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 from drivers.hokuyo.hokuyo import hokuyo
 from drivers.driver import VariableOperation, VariableDatatype
+
+""" Test client to communicate with driver"""
+def client(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (ip, port)
+
+    print(f'Client starting, connecting to {server_address}')
+    sock.connect(server_address)
+    print(f'Connected to {server_address}')
+
+    while True:
+        message = b'\x02000EAR01B19B\x03'
+        sock.send(message)
+        # Wait for response
+        try:
+            t1 = time.perf_counter()
+            response = sock.recv(9000)
+            resp_time = round((time.perf_counter() - t1)*1000)
+            print(f'Client received {len(response)} bytes of data after {resp_time} ms')
+        except Exception as e:
+            print(e)
+            sock.close()
+            print('Client disconnected')
+            break
+""" ------------------------------------"""
+
+_client = threading.Thread(target=client, args=("127.0.0.1", 10940))
+_client.start()
 
 VARIABLES = {
     '0_134': {'datatype': VariableDatatype.INTEGER, 'size': 135, 'operation': VariableOperation.WRITE},
@@ -35,6 +65,7 @@ VARIABLES = {
     '945_1080': {'datatype': VariableDatatype.INTEGER, 'size': 136, 'operation': VariableOperation.WRITE},
     }
 
+
 # Curve that the points will follow during test
 def f(t):
     y = [0] * 1081
@@ -42,6 +73,7 @@ def f(t):
         y[x] = np.sin(t*2 - x/40)*1000+1500
         y[x] = int(y[x])
     return y
+
 
 d = hokuyo(None, 'test')
 if d.connect():
@@ -51,7 +83,7 @@ if d.connect():
     last_update = 0
     while True:
         now = time.perf_counter()
-        if now - start_time > 10:
+        if now - start_time > 5:
             break
 
         # Simulate running the loop
