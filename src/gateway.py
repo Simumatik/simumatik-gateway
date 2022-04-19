59449d3cc0e7c9a0474590c236808a7f850f45c0
Mikel Ayani <mikel.ayani@simumatik.com>
2022-04-19 13:27:44 +0200

    v3.2.2 Fixed local folder path

--- src/gateway.py
+++ src/gateway.py
@@ -30,7 +30,7 @@ from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 
 
 # Version
-version = "3.2.1"
+version = "3.2.2"
 
 # Settings
 poll_time = 1 # seconds
--- src/logs.py
+++ src/logs.py
@@ -2,7 +2,13 @@
 import logging
 import os
 
-LOCAL_FOLDER =  f"{os.path.expanduser('~')}/Simumatik"
+ACTUAL_PATH = os.path.dirname(os.path.abspath(__file__))
+USER_PATH = os.path.expanduser('~')
+if USER_PATH in ACTUAL_PATH:
+    LOCAL_FOLDER = os.path.dirname(ACTUAL_PATH) # Works for different branches
+else:
+    LOCAL_FOLDER =  f"{USER_PATH}/Simumatik"
+
 FORMAT = '%(asctime)-15s %(levelname)s %(name)s: %(message)s'
 logging.basicConfig(filename=f"{LOCAL_FOLDER}/gateway.log",
                     filemode='w',
