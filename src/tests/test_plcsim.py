5bd9f0571cf80fdd1ab2c2d718ab8f0c0ae0bb49
Mikel Ayani <mikel.ayani@simumatik.com>
2022-09-13 17:08:35 +0200

    test using ctypes

---
+++ src/tests/test_plcsim.py
@@ -0,0 +1,30 @@
+# Simumatik Gateway - Simumatik 3rd party integration tool
+# Copyright (C) 2021 Simumatik AB
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import sys
+from os import path
+import time
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+import ctypes
+plcsim = ctypes.WinDLL('s7onlinx.dll')
+e = plcsim.SCP_get_errno()
+print(e)
+conn = plcsim.SCP_open("S7ONLINE")
+print(conn)
+e = plcsim.SCP_get_errno()
+print(e)
+plcsim.SCP_close()
