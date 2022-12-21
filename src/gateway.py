ff8e758e0456def399cfc39fb56bd75007b6a916
Mikel Ayani <mikel.ayani@simumatik.com>
2022-12-21 09:26:39 +0100

    v4.0.11

--- src/gateway.py
+++ src/gateway.py
@@ -30,7 +30,7 @@ from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 
 
 # Version
-version = "4.0.10"
+version = "4.0.11"
 MAX_TELEGRAM_LENGTH = 2**13 # (8K)
 MAX_UPDATES_PER_TELEGRAM = 100
 
