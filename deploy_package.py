a1218f5ccd257a4fe64a405b0e651c6b2ef80193
Mikel Ayani <mikel.ayani@simumatik.com>
2023-10-10 11:37:59 +0200

    v4.3.1 New certificate added

--- deploy_package.py
+++ deploy_package.py
@@ -17,13 +17,12 @@ print(f"Deploying {PACKAGE} package for platform {PLATFORM}...")
 # WINDOWS
 if PLATFORM=="win32":
     SIGNTOOLS_PATH = "\"C:/Program Files (x86)/Windows Kits/10/bin/10.0.22000.0/x86/signtool.exe\""
-    TIMESTAMP_URL = "http://timestamp.digicert.com"
-    SIGNING_CERT = "SimumatikCodeSigningCertificate.pfx"
+    TIMESTAMP_URL = "http://timestamp.comodoca.com"
     PACKAGE_PATH = 'C:/Git/'
     PACKAGE_SPEC_PATH = f'{PACKAGE_PATH}simumatik-gateway/gateway_package.spec'
     PYTHON_PATH = 'C:/python311-32/python.exe'
     subprocess.call(f'{PYTHON_PATH} -m PyInstaller --noconfirm {PACKAGE_SPEC_PATH}', shell=True)
-    subprocess.call(f"{SIGNTOOLS_PATH} sign /f {SIGNING_CERT} /fd SHA256 /p 11dce81f9e5a893ca23cc020da45157c85ed93a5 /t {TIMESTAMP_URL} dist/{PACKAGE}/{PACKAGE_EXECUTABLE}", shell=True)
+    subprocess.call(f"{SIGNTOOLS_PATH} sign /tr {TIMESTAMP_URL} /td sha256 /fd sha256 /a dist/{PACKAGE}/{PACKAGE_EXECUTABLE}", shell=True)
 
 # LINUX
 elif PLATFORM=="linux":
