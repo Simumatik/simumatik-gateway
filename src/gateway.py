e9c0cde3516471af23cc5469fe159df753f4821a
Mikel Ayani <mikel.ayani@simumatik.com>
2024-07-30 23:35:42 +0200

    v5.0.7 pyads library updated

--- src/gateway.py
+++ src/gateway.py
@@ -27,7 +27,7 @@ from gateway_ws_interface import GatewayWsInterface, GatewayWsCommands
 from workspace_udp_interface import WorkspaceUDPInterface, WorkspaceCommand, MINIMUM_SYNC_PERIOD, STANDBY_SYNC_PERIOD
 
 # Version
-version = "5.0.6"
+version = "5.0.7"
 
 MAX_PIPE_LOOPS = 10
 '''
