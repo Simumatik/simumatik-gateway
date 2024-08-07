7f8fcafba5b3dfc834955b55e5bb71a1b072147b
Mikel Ayani <mikel.ayani@simumatik.com>
2024-08-07 11:46:42 +0200

    v5.1.1

--- src/gateway.py
+++ src/gateway.py
@@ -28,7 +28,7 @@ from gateway_ws_interface import GatewayWsInterface, GatewayWsCommands
 from workspace_udp_interface import WorkspaceUDPInterface, WorkspaceCommand, MINIMUM_SYNC_PERIOD, STANDBY_SYNC_PERIOD
 
 # Version
-version = "5.0.7"
+version = "5.1.1"
 
 MAX_PIPE_LOOPS = 10
 '''
