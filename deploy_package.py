15d857c961a143dd8d5146e539d10e4d64ed2b09
Mikel Ayani <mikel.ayani@simumatik.com>
2024-09-04 21:57:27 +0200

    Format fix in deploy script

--- deploy_package.py
+++ deploy_package.py
@@ -1,8 +1,8 @@
+""" TODO """
 import sys
 import shutil
 import subprocess
 import asyncio
-import shutil
 
 from simumatik_api_helper import UploadFile, GetRequestJson, GetSimumatikApiToken
 
@@ -11,7 +11,7 @@ OS_EXECUTABLE_EXT = {"win32": "exe", "linux": "sh", "darwin": "sh"}
 
 PACKAGE = 'Gateway'
 PACKAGE_EXECUTABLE = f'{PACKAGE}.{OS_EXECUTABLE_EXT[PLATFORM]}'
-    
+
 print(f"Deploying {PACKAGE} package for platform {PLATFORM}...")
 
 # WINDOWS
@@ -22,7 +22,10 @@ if PLATFORM=="win32":
     PACKAGE_SPEC_PATH = f'{PACKAGE_PATH}simumatik-gateway/{PACKAGE}_package.spec'
     PYTHON_PATH = 'C:/python312-32/python.exe'
     subprocess.call(f'{PYTHON_PATH} -m PyInstaller --noconfirm {PACKAGE_SPEC_PATH}', shell=True)
-    subprocess.call(f"{SIGNTOOLS_PATH} sign /tr {TIMESTAMP_URL} /td sha256 /fd sha256 /a dist/{PACKAGE}/{PACKAGE_EXECUTABLE}", shell=True)
+    subprocess.call(
+        f"{SIGNTOOLS_PATH} sign /tr {TIMESTAMP_URL} /td sha256 /fd sha256 /a dist/{PACKAGE}/{PACKAGE_EXECUTABLE}",
+        shell=True
+        )
 
 # LINUX
 elif PLATFORM=="linux":
@@ -39,26 +42,60 @@ experimental = input() == '1'
 
 # TODO: Fix new paths for other platforms
 if experimental:
-    actual_version = asyncio.run(GetRequestJson(token=TOKEN, url=f'{AUDIENCE}/{PACKAGE.lower()}/experimental/version'))
+    actual_version = asyncio.run(
+        GetRequestJson(
+            token=TOKEN, 
+            url=f'{AUDIENCE}/{PACKAGE.lower()}/experimental/version'
+            )
+        )
 else:
-    actual_version = asyncio.run(GetRequestJson(token=TOKEN, url=f'{AUDIENCE}/{PACKAGE.lower()}/version'))
+    actual_version = asyncio.run(
+        GetRequestJson(
+            token=TOKEN, 
+            url=f'{AUDIENCE}/{PACKAGE.lower()}/version'
+            )
+        )
 
 print(f"Actual {PACKAGE} package version is: {actual_version}")
 print("[+] Introduce the new version number (i.e. '1.0.0'): ")
 version = input()
-filepath = 'Output/'
+FILEPATH = 'Output/'
 
 print("Ziping the package...")
-shutil.make_archive(f'{filepath}{PACKAGE}-{version}', 'zip', f'dist/{PACKAGE}')
+shutil.make_archive(f'{FILEPATH}{PACKAGE}-{version}', 'zip', f'dist/{PACKAGE}')
 filename = f'{PACKAGE}-{version}.zip'
 print('Uploading package...')
 if experimental:
-    asyncio.run(UploadFile(token=TOKEN, url=f'{AUDIENCE}/admin/{PACKAGE.lower()}/experimental', path=filepath, filename=filename))
+    asyncio.run(
+        UploadFile(
+            token=TOKEN, 
+            url=f'{AUDIENCE}/admin/{PACKAGE.lower()}/experimental', 
+            path=FILEPATH, 
+            filename=filename
+            )
+        )
 else:
-    asyncio.run(UploadFile(token=TOKEN, url=f'{AUDIENCE}/admin/{PACKAGE.lower()}', path=filepath, filename=filename))
+    asyncio.run(
+        UploadFile(
+            token=TOKEN, 
+            url=f'{AUDIENCE}/admin/{PACKAGE.lower()}', 
+            path=FILEPATH, 
+            filename=filename
+            )
+        )
 print('Package uploaded!')
 if experimental:
-    version = asyncio.run(GetRequestJson(token=TOKEN, url=f'{AUDIENCE}/{PACKAGE.lower()}/experimental/version'))
+    version = asyncio.run(
+        GetRequestJson(
+            token=TOKEN,
+            url=f'{AUDIENCE}/{PACKAGE.lower()}/experimental/version'
+            )
+        )
 else:
-    version = asyncio.run(GetRequestJson(token=TOKEN, url=f'{AUDIENCE}/{PACKAGE.lower()}/version'))
+    version = asyncio.run(
+        GetRequestJson(
+            token=TOKEN,
+            url=f'{AUDIENCE}/{PACKAGE.lower()}/version'
+            )
+        )
 print(f"Uploaded {PACKAGE} package version is: {version}")
